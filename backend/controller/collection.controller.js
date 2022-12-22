b46dbac6946b4a4e4aaa33fda9f8dcc6d66aba78
melto.sm <meltosm8@gmail.com>
2022-12-21 17:41:32 -0800

    update collection is added to the controller

--- backend/controller/collection.controller.js
+++ backend/controller/collection.controller.js
@@ -10,7 +10,6 @@ import CustomError from '../utlils/customError'
  * @parameters name
  * @return success message, collections object
  ************************************************************/
-
 export const createCollection = asyncHandler(async (req, res) => {
     const { name } = req.body
 
@@ -26,5 +25,43 @@ export const createCollection = asyncHandler(async (req, res) => {
         success: true,
         message: "collection created successfully",
         collection
+    })
+})
+
+/*************************************************************
+ * @Update_Collection
+ * @Method POST
+ * @Route http://localhost:5000/api/updatecollection
+ * @description User update collections
+ * @parameters name
+ * @return success message, collections object
+ ************************************************************/
+export const updateCollection = asyncHandler(async () => {
+    const { id: collectionID } = req.params
+    const { name } = req.body
+
+    if(!name) {
+        throw new CustomError('Name field is required', 400)
+    }
+
+    const updatedCollection = await Collection.findByIdAndUpdate(
+        collectionID,
+        {
+            name
+        },
+        {
+            new: true, // it will return the newly updated value
+            runValidators: true
+        }
+    )
+
+    if(!updatedCollection) {
+        throw new CustomError('Collection not found', 400)
+    }
+    
+    res.status(200).json({
+        success: true,
+        message: "Successfully updated",
+        updatedCollection
     })
 })
