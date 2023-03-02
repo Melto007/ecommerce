5c2d8bbdfa3084e93fb2bc5d876299764884b989
melto.sm <meltosm8@gmail.com>
2023-03-02 07:42:12 -0800

    database config is created

---
+++ index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import app from './app'
+import config from './config/index'
+
+(async () => {
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("DB is connected successfully")
+
+        app.on('error', (error) => {
+            console.log("ERROR ", error)
+            throw error
+        })
+
+        const onListening = () => {
+            console.log(`APP is running on PORT ${config.PORT}`)
+        }
+
+        app.listen(config.PORT, onListening)
+    } catch (error) {
+        console.log("ERROR ", error)
+        throw error
+    }
+})()
