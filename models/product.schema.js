5fc6163cbc106c14539857d539e53100c7cd11e9
melto.sm <meltosm8@gmail.com>
2023-03-01 18:08:08 -0800

    product schema is created

--- models/product.schema.js
+++ models/product.schema.js
@@ -5,7 +5,36 @@ const productSchema = new mongoose.Schema(
         name: {
             type: String,
             required: [true, "Product name is required"],
+            trim: true,
             maxLength: [120, "Length of the product name must be less than 120 characters"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Product price is required"],
+            maxLength: [6, "Length of the product price must be less than 6 digits"]
+        },
+        description: {
+            type: String
+        },
+        photos: [
+            {
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        stock: {
+            type: Number,
+            default: 0
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        collectionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Collection"
         }
     },
     {
