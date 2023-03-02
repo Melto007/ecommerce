cc7dd80514d3418dba8291d731b04c53aa0eca2c
melto.sm <meltosm8@gmail.com>
2023-03-01 18:58:35 -0800

    order schema is created

---
+++ models/orderSchema.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema(
+    {
+        products: {
+            type: [
+                {
+                    productId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Product",
+                        required: true
+                    },
+                    count: Number,
+                    price: Number
+                }
+            ],
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+export default mongoose.model("Order", orderSchema)
