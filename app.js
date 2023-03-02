4d6a61d2a20bdb14b9b4434cd53529c819140ff8
melto.sm <meltosm8@gmail.com>
2023-03-02 09:07:10 -0800

    database is configured

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(cors())
+
+app.use(morgan('tiny'))
 
 export default app
